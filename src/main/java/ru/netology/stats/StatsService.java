01ba0b0d7f39e5ebf8038cf6bca0d6daa232491d
Name SurnameAlexander <mitloony@yandex.ru>
2025-08-17 22:35:00 +0300

    v2

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -5,22 +5,19 @@ public class StatsService {
     public long getTotalSales(long[] sales) {
         long total = 0;
         for (long sale : sales) {
-            total = total + sale;
+            total += sale;
         }
         return total;
     }
 
     public long getMiddleSales(long[] sales) {
-        long total1 = 0;
-        long total = getTotalSales(sales);
-        total1 = total / (sales.length);
-        return total1;
+        return getTotalSales(sales) / sales.length;
     }
 
     public long getMaxSales(long[] sales) {
         int HighMonth = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] > sales[HighMonth]) {
+            if (sales[i] >= sales[HighMonth]) {
                 HighMonth = i;
             }
         }
@@ -38,7 +35,7 @@ public class StatsService {
     }
 
     public long countMonthsUnderAverage(long[] sales) {
-        long average = getTotalSales(sales) / sales.length;
+        long average = getMiddleSales(sales);  // вызываем уже готовый метод по расчету средней продажи
         long count = 0;
         for (long sale : sales) {
             if (sale < average) {
@@ -49,14 +46,14 @@ public class StatsService {
     }
 
     public long countMonthsAboveAverage(long[] sales) {
-        long average = getTotalSales(sales) / sales.length;
-        long count1 = 0;
+        long average = getTotalSales(sales) / sales.length; // можно и так
+        long count = 0;
         for (long sale : sales) {
             if (sale > average) {
-                count1++;
+                count++;
             }
         }
-        return count1;
+        return count;
     }
 
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -28,7 +28,7 @@ public class StatsServiceTest {
     public void maxsumm() {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 6;
+        long expected = 8;
         long actual = service.getMaxSales(sales);
         Assertions.assertEquals(expected, actual);
     }
